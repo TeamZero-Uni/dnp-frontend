7cb7ac62d943ab4502d570e8a0e6c7579657ab44
dilushamadushan <dmdmadushan007@gmail.com>
2026-02-09 23:20:21 +0530

    Add main layout components and routing for the application
    - Updated index.html title to "DNP 3D Hobby Lobby"
    - Added logo image to assets
    - Implemented Navbar and Footer components with responsive design
    - Created About, Services, Product, Login, and Register pages
    - Added LoginForm component for user authentication
    - Introduced Banner component for the About page
    - Enhanced MainLayout to conditionally render Navbar and Footer based on route
    - Added CSS animations for smoother UI transitions

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,26 @@
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
-import { Outlet } from "react-router-dom";
+import { useLocation, Outlet } from "react-router-dom";
 
 
 function MainLayout() {
+  const location = useLocation();
+
+  const hideheaderFooterRoutes = ["/login", "/register", "/dash"];
+
+  const shouldHide = hideheaderFooterRoutes.some((path) => {
+    location.pathname.toLowerCase().startsWith(path.toLowerCase());
+  });
+
   return (
     <>
-      <Navbar />
-      <Outlet />
-      <Footer />
+      <div className="text-white min-h-screen flex flex-col">
+        {!shouldHide && <Navbar />}
+        <main className="grow min-h-screen mt-23 text-black">
+          <Outlet />
+        </main>
+        {!shouldHide && <Footer />}
+      </div>
     </>
   );
 }
