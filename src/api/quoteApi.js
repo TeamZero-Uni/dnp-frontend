3d6a7e3d6cc4e55bd6f3a6c9551ec5f2a3346ab2
dilushamadushan <dmdmadushan007@gmail.com>
2026-05-13 23:19:15 +0530

    Refactor quote management: integrate API for fetching quotes, enhance status handling, and improve UI components for better user experience.

--- src/api/quoteApi.js
+++ src/api/quoteApi.js
@@ -1,7 +1,11 @@
-import api from "./api";
+import api, { request } from "./api";
 
 //add quote request data
 export const submitQuoteRequest = async (quoteData) => {
     const response = await api.post("/quote", quoteData);
     return response.data;
 }
+
+export const getQuotetions = async () => request("get", "/quote");
+
+export const updateQuotetion = async (data) => request("put", `/quote/${data.q_id}`, data); 
