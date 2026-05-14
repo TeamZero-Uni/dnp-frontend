e3ee2fd03865d26f1229b4843ad9f29bb588efcf
maleeshachathuranga <maleesha16chathuranga@gmail.com>
2026-05-14 09:37:21 +0530

    Add submitFeedback and getApprovedFeedbacks functions to feedbackApi

--- src/api/feedbackApi.js
+++ src/api/feedbackApi.js
@@ -1,6 +1,8 @@
 import api, { request } from "./api";
 
+export const submitFeedback = async ({ message, rating }) => request("post", "/feedback", { message, rating });
 export const getAllFeedback = async () => request("get", "/feedback");
+export const getApprovedFeedbacks = async () => request("get", "/feedback/approved");
 export const deleteFeedback = async (id) => {
         const res = await api.delete(`/feedback/${id}`);
         return res.data;
